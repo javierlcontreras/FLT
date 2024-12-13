0fac4ed519ec311046ecbddf33ba9c4462ad9d9c
Javier López-Contreras <javierlc2000@gmail.com>
2024-12-13 02:36:29 +0000

    Add Representation Irreducible and Absdolutely Irreducible

--- FLT/Mathlib/RepresentationTheory/Basic.lean
+++ FLT/Mathlib/RepresentationTheory/Basic.lean
@@ -3,10 +3,18 @@ import Mathlib.RepresentationTheory.Basic
 
 universe u
 
-def GL_map_of_representation_of_basis {R V G ι: Type u} [CommRing R] [AddCommMonoid V] [Module R V]
+open scoped TensorProduct
+
+def Representation.GL_map_of_representation_of_basis {R V G ι: Type u} [CommRing R] [AddCommMonoid V] [Module R V]
   [Module.Free R V] [Module.Finite R V] [Group G] [DecidableEq ι] [Fintype ι]
   (ρ : Representation R G V) (𝓑 : Basis ι R V)
   : G →* Matrix.GeneralLinearGroup ι R :=
   sorry
 
-#min_imports
+/- Tensor product between a representation and a extension of constants -/
+def Representation.tprod' {k k' : Type*} {G : Type*} {W' : Type*}
+  [Monoid G]
+  [CommSemiring k] [CommSemiring k'] [Algebra k' k]
+  [AddCommMonoid W'] [Module k' W']
+  (ρV : Representation k' G W') : Representation k G (k ⊗[k'] W')  :=
+  sorry
