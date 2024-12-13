0df5d9506984afb92c51ca7212d7924cc1dd7659
Javier López-Contreras <javierlc2000@gmail.com>
2024-12-13 07:59:44 +0000

    Fixes build. Introduces Subrepresentations and RepresentationEquiv

--- FLT/Mathlib/RepresentationTheory/Basic.lean
+++ FLT/Mathlib/RepresentationTheory/Basic.lean
@@ -1,20 +1,35 @@
-import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 import Mathlib.RepresentationTheory.Basic
-
-universe u
+import FLT.Mathlib.RepresentationTheory.Subrepresentation
 
 open scoped TensorProduct
 
-def Representation.GL_map_of_representation_of_basis {R V G ι: Type u} [CommRing R] [AddCommMonoid V] [Module R V]
+namespace Representation
+
+def GL_map_of_representation_of_basis {R V G ι: Type*} [CommRing R] [AddCommMonoid V] [Module R V]
   [Module.Free R V] [Module.Finite R V] [Group G] [DecidableEq ι] [Fintype ι]
   (ρ : Representation R G V) (𝓑 : Basis ι R V)
   : G →* Matrix.GeneralLinearGroup ι R :=
   sorry
 
-/- Tensor product between a representation and a extension of constants -/
-def Representation.tprod' {k k' : Type*} {G : Type*} {W' : Type*}
-  [Monoid G]
-  [CommSemiring k] [CommSemiring k'] [Algebra k' k]
-  [AddCommMonoid W'] [Module k' W']
-  (ρV : Representation k' G W') : Representation k G (k ⊗[k'] W')  :=
-  sorry
+variable {G : Type*} [Group G]
+
+variable {A : Type*} [CommRing A]
+
+variable {W : Type*} [AddCommMonoid W] [Module A W]
+
+noncomputable def tprod' (A' : Type*) [CommRing A'] [Algebra A A']
+  (ρ : Representation A G W): Representation A G (A' ⊗[A] W) := sorry
+
+notation R "⊗ᵣ[" A "]" ρ => Representation.tprod' (A := A) R ρ
+notation ρ "⊗ᵣ" ρ' => Representation.tprod ρ ρ'
+
+class Irreducible (ρ : Representation A G W) : Prop where
+  irreducible : ∀ ρ' ρ'' : Subrepresentation A G W, ∃ _ : ρ' ⊓ ρ'' = ⊥,
+      ρ = ρ' ⊔ ρ'' → ρ' = ⊤ ∨ ρ'' = ⊤
+
+class AbsolutelyIrreducible (ρ : Representation A G W) : Prop where
+  absolutelyIrreducible :
+    ∀ A', ∃ _ : CommRing A', ∀ _ : Algebra A A',
+    Irreducible (A' ⊗ᵣ[A] ρ)
+
+end Representation
