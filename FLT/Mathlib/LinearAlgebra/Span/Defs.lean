4cd179ca7a0241bc2e2668cca1d11db1386c2318
Javier López-Contreras <javierlc2000@gmail.com>
2024-12-16 08:02:05 +0000

    feat(RepresentationTheory/Subrepresentation): Adds subreps and simple reps

---
+++ FLT/Mathlib/LinearAlgebra/Span/Defs.lean
@@ -0,0 +1,18 @@
+import Mathlib.LinearAlgebra.Span.Defs
+
+variable {R : Type*} [Semiring R]
+
+variable {M : Type*} [AddCommMonoid M] [Module R M]
+
+variable {p p' : Submodule R M}
+
+variable {P : M → Prop}
+
+@[simp high]
+lemma Submodule.forall_mem_sup : (∀ x ∈ p ⊔ p', P x) ↔ (∀ x₁ ∈ p, ∀ x₂ ∈ p', P (x₁ + x₂)) := by
+  simp [Submodule.mem_sup]
+  aesop
+
+@[simp high]
+lemma Submodule.exists_mem_sup : (∃ x ∈ p ⊔ p', P x) ↔ (∃ x₁ ∈ p, ∃ x₂ ∈ p', P (x₁ + x₂)) := by
+  simp [Submodule.mem_sup]
--- FLT/Mathlib/RepresentationTheory/Basic.lean
+++ FLT/Mathlib/RepresentationTheory/Basic.lean
@@ -1,7 +1,6 @@
+import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
 import Mathlib.RepresentationTheory.Basic
-import FLT.Mathlib.RepresentationTheory.Subrepresentation
 
-open scoped TensorProduct
 
 namespace Representation
 
@@ -11,25 +10,5 @@ def GL_map_of_representation_of_basis {R V G ι: Type*} [CommRing R] [AddCommMon
   : G →* Matrix.GeneralLinearGroup ι R :=
   sorry
 
-variable {G : Type*} [Group G]
-
-variable {A : Type*} [CommRing A]
-
-variable {W : Type*} [AddCommMonoid W] [Module A W]
-
-noncomputable def tprod' (A' : Type*) [CommRing A'] [Algebra A A']
-  (ρ : Representation A G W): Representation A G (A' ⊗[A] W) := sorry
-
-notation R "⊗ᵣ[" A "]" ρ => Representation.tprod' (A := A) R ρ
-notation ρ "⊗ᵣ" ρ' => Representation.tprod ρ ρ'
-
-class Irreducible (ρ : Representation A G W) : Prop where
-  irreducible : ∀ ρ' ρ'' : Subrepresentation A G W, ∃ _ : ρ' ⊓ ρ'' = ⊥,
-      ρ = ρ' ⊔ ρ'' → ρ' = ⊤ ∨ ρ'' = ⊤
-
-class AbsolutelyIrreducible (ρ : Representation A G W) : Prop where
-  absolutelyIrreducible :
-    ∀ A', ∃ _ : CommRing A', ∀ _ : Algebra A A',
-    Irreducible (A' ⊗ᵣ[A] ρ)
 
 end Representation
